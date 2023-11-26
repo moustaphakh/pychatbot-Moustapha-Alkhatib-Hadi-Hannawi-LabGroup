952ed74a4722e19b3ad348f9f497a5e34bb5e446
moustaphakh <152089001+moustaphakh@users.noreply.github.com>
2023-11-26 20:36:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,52 @@ def list_of_files(directory, extension):
             files_names.append(filename)   
     return files_names
 
+#speeches is a list of the files in the directory speeches
+speeches = list_of_files("speeches",'txt')
+
+
+#extract_president_names function extracts the names of the presidents from the speech txt files names
+def extract_president_names():
+    """this function extracts the names of the presidents from the speech text files"""
+    president_names = set()
+    for text in speeches:
+        if text[-5] == '1' or text[-5] == '2':
+            president_names.add(text[11:-5])
+        else:
+            president_names.add(text[11:-4])
+    president_names = list(president_names)
+    return president_names
+
+#last_names is a list containing the names of the presidents by extracting the names from the speech filenames
+last_names = extract_president_names()
+
+#this function creates a dictionary associating each president's first name with his last
+def associate_first_names():
+    """this function creates a dictionary that associates to each president his first and last name"""
+    names = {
+        'Chirac': 'Jacques',
+        'Giscard dEstaing': 'Valery',
+        'Mitterrand': 'Francois',
+        'Macron': 'Emmanuel',
+        'Sarkozy': 'Nicolas',
+        'Hollande' :'Francois'
+    }
+
+    return {president: names[president] for president in last_names}
+#full_names is a dictionary of the presidents first and last names
+full_names = associate_first_names()
+
+#this function displays the names of the presidents
+def display_names():
+    """this function displays the names of the presidents"""
+    for name in full_names:
+        print(full_names[name],name)
+
+
+
+
+
+
 
 
 
